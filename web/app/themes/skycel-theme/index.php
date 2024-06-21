89494ed5149b13b6b797f9719151b270fc6108ce
Célian <celian@qs.team>
2024-06-21 20:43:35 +0200

    Commit de rendu

--- web/app/themes/skycel-theme/index.php
+++ web/app/themes/skycel-theme/index.php
@@ -1,57 +1,33 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package skycel-theme
- */
+wp_head();
 
-get_header();
-?>
+$tracks = json_decode(file_get_contents("data/circuits.json"));
+$races = json_decode(file_get_contents("data/races.json"));
+$teams = json_decode(file_get_contents("data/teams.json"));
+$drivers = json_decode(file_get_contents("data/drivers.json"));
 
-	<main id="primary" class="site-main">
+$args = array(
+	"post_status"=> "publish",
+	"post_type"=> "circuit",
+	"post_title"=> $tracks[0]->name,
+);
 
-		<?php
-		if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+//$id = wp_insert_post($args);
+$id = 102;
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+$place = array(
+	"city"=>$tracks[0]->place->city,
+	"country"=>$tracks[0]->place->country
+);
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+$usage = array("years"=> array(
+	"range_start"=>$tracks[0]->usage->start,
+	"range_end"=>$tracks[0]->usage->end
+), "year"=> null);
 
-			endwhile;
+$lapDistance = $tracks[0]->lapDistance->kilometers;
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+update_field("place", $place, $id);
+update_field("usage", $usage, $id);
+update_field("distance", $lapDistance, $id);
